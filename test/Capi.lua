81a1bfe609c12f9617550cb0d2ad700c89757ad5
rui <rui1105@outlook.com>
2020-04-22 21:28:11 +0800

    lua_002

---
+++ test/Capi.lua
@@ -0,0 +1,6 @@
+-- Scheme compiler git clone https://github.com/cisco/ChezScheme
+-- Scheme first www.ibm.com/developerworks/cn/linux/l-schm/index1.html
+
+-- Haskell functional programme language
+-- www.haskell.org
+lua_State
